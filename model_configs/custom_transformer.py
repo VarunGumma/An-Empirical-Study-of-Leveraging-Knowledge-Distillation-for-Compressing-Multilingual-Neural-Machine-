bd1d78c15eebe067e257a54f4f6535d72f6027be
VarunGumma <varun230999@gmail.com>
2022-12-05 15:03:37 +0530

    added transformer_2x_v0

--- model_configs/custom_transformer.py
+++ model_configs/custom_transformer.py
@@ -29,6 +29,21 @@ def transformer_big(args):
     base_architecture(args)
 
 
+@register_model_architecture("transformer", "transformer_2x_v0")
+def transformer_big(args):
+    args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 1024)
+    args.encoder_ffn_embed_dim = getattr(args, "encoder_ffn_embed_dim", 4096)
+    args.encoder_attention_heads = getattr(args, "encoder_attention_heads", 16)
+    args.encoder_normalize_before = getattr(args, "encoder_normalize_before", True)
+    args.decoder_embed_dim = getattr(args, "decoder_embed_dim", 1024)
+    args.decoder_ffn_embed_dim = getattr(args, "decoder_ffn_embed_dim", 4096)
+    args.decoder_attention_heads = getattr(args, "decoder_attention_heads", 16)
+    args.decoder_normalize_before = getattr(args, "decoder_normalize_before", True)
+    args.activation_fn = getattr(args, "activation_fn", "gelu")
+    args.layernorm_embedding = getattr(args, "layernorm_embedding", True)
+    base_architecture(args)
+
+
 @register_model_architecture("transformer", "transformer_4x")
 def transformer_huge(args):
     args.encoder_embed_dim = getattr(args, "encoder_embed_dim", 1536)
