8cdc3024b73eaaac6aab71fb2e0a86fcbbb7ea75
Varun Gumma <varun230999@gmail.com>
2023-01-25 10:57:16 +0530

    .

--- training_scripts/apply_fisher_masks.sh
+++ training_scripts/apply_fisher_masks.sh
@@ -5,7 +5,7 @@ fairseq-train ../../data_bin/v2_distilled_indic_en_bin/final_bin \
 --max-target-positions 210 \
 --max-update 1000000 \
 --arch transformer_1x_v0 \
---task translation_with_fisher_masks \
+--task translation_apply_fisher_masks \
 --criterion label_smoothed_cross_entropy \
 --source-lang SRC \
 --target-lang TGT \
@@ -21,15 +21,10 @@ fairseq-train ../../data_bin/v2_distilled_indic_en_bin/final_bin \
 --save-interval-updates 5000 \
 --patience 5 \
 --skip-invalid-size-inputs-valid-test \
---update-freq 8 \
+--update-freq 3 \
 --distributed-world-size 1 \
-<<<<<<< HEAD
 --max-tokens 8192 \
---lr 5e-4 \
-=======
---max-tokens 2048 \
---lr 3e-5 \
->>>>>>> 8601bdf (.)
+--lr 7e-4 \
 --restore-file ../checkpoints/base/checkpoint_best.pt \
 --reset-lr-scheduler \
 --reset-meters \
