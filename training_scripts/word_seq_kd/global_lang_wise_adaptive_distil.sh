b03e4e5e6c9920d1e2a9587ba816920febe3a729
VarunGumma <varun230999@gmail.com>
2023-03-01 18:13:56 +0530

    fixed scripts

--- training_scripts/word_seq_kd/global_lang_wise_adaptive_distil.sh
+++ training_scripts/word_seq_kd/global_lang_wise_adaptive_distil.sh
@@ -10,13 +10,14 @@ fairseq-train ../../../data_bin/v2_distilled_indic_en_bin/final_bin \
 --layernorm-embedding \
 --dropout 0.2 \
 --task translation_with_kd \
---kd-strategy global_multi_level \
+--kd-strategy global_language_wise \
 --teacher-checkpoint-path ../../checkpoints/it/checkpoint_best.pt \
 --criterion label_smoothed_cross_entropy_with_kd \
 --label-smoothing 0.1 \
 --alpha 0.5 \
---kd-rate 0.5 \
+--use-adaptive-kd-rates \
 --kd-queue-size 5000000 \
+--kd-selection-temp 2.5 \
 --source-lang SRC \
 --target-lang TGT \
 --lr-scheduler inverse_sqrt \
@@ -26,15 +27,15 @@ fairseq-train ../../../data_bin/v2_distilled_indic_en_bin/final_bin \
 --warmup-init-lr 1e-07 \
 --lr 0.0005 \
 --warmup-updates 4000 \
---save-dir ../../checkpoints/global_multi_distil \
+--save-dir ../../checkpoints/global_multi_adaptive_distil \
 --save-interval 1 \
 --save-interval-updates 5000 \
 --keep-interval-updates 1 \
 --no-epoch-checkpoints \
 --patience 5 \
 --skip-invalid-size-inputs-valid-test \
---update-freq 1 \
---distributed-world-size 8 \
+--update-freq 8 \
+--distributed-world-size 1 \
 --num-workers 32 \
 --eval-bleu \
 --eval-bleu-args '{"beam": 5, "lenpen": 1.0, "max_len_a": 1.2, "max_len_b": 10}' \
@@ -43,5 +44,4 @@ fairseq-train ../../../data_bin/v2_distilled_indic_en_bin/final_bin \
 --eval-bleu-print-samples \
 --best-checkpoint-metric bleu \
 --maximize-best-checkpoint-metric \
---wandb-project Indic-En-Distillation \
 --memory-efficient-fp16
